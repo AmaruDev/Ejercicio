15dc217481bdfbf217b7ab918a0c137b7d18da0c
amand <amand@LAPTOP-Q5R5O6T5>
2022-05-23 00:11:50 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Ejercicio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.idAutoBox = new System.Windows.Forms.TextBox();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.marcaBox = new System.Windows.Forms.TextBox();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_actualizar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSetAutos = new Ejercicio.DataSetAutos();
+            this.dataSetAutosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.autosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.autosTableAdapter = new Ejercicio.DataSetAutosTableAdapters.AutosTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.marcaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetAutos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetAutosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.autosBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Auto";
+            // 
+            // idAutoBox
+            // 
+            this.idAutoBox.Location = new System.Drawing.Point(168, 57);
+            this.idAutoBox.Name = "idAutoBox";
+            this.idAutoBox.Size = new System.Drawing.Size(100, 20);
+            this.idAutoBox.TabIndex = 1;
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.Location = new System.Drawing.Point(101, 241);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(107, 23);
+            this.btn_agregar.TabIndex = 2;
+            this.btn_agregar.Text = "Ingresar Auto";
+            this.btn_agregar.UseVisualStyleBackColor = true;
+            this.btn_agregar.Click += new System.EventHandler(this.btn_agregar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Marca";
+            // 
+            // marcaBox
+            // 
+            this.marcaBox.Location = new System.Drawing.Point(168, 137);
+            this.marcaBox.Name = "marcaBox";
+            this.marcaBox.Size = new System.Drawing.Size(100, 20);
+            this.marcaBox.TabIndex = 4;
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Location = new System.Drawing.Point(243, 241);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(107, 23);
+            this.btn_eliminar.TabIndex = 5;
+            this.btn_eliminar.Text = "Eliminar Auto";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(243, 295);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(107, 23);
+            this.btn_limpiar.TabIndex = 6;
+            this.btn_limpiar.Text = "Limpiar Campos";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_actualizar
+            // 
+            this.btn_actualizar.Location = new System.Drawing.Point(101, 295);
+            this.btn_actualizar.Name = "btn_actualizar";
+            this.btn_actualizar.Size = new System.Drawing.Size(107, 23);
+            this.btn_actualizar.TabIndex = 7;
+            this.btn_actualizar.Text = "Actualizar Auto";
+            this.btn_actualizar.UseVisualStyleBackColor = true;
+            this.btn_actualizar.Click += new System.EventHandler(this.btn_actualizar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.marcaDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.autosBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(413, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 261);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // dataSetAutos
+            // 
+            this.dataSetAutos.DataSetName = "DataSetAutos";
+            this.dataSetAutos.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataSetAutosBindingSource
+            // 
+            this.dataSetAutosBindingSource.DataSource = this.dataSetAutos;
+            this.dataSetAutosBindingSource.Position = 0;
+            // 
+            // autosBindingSource
+            // 
+            this.autosBindingSource.DataMember = "Autos";
+            this.autosBindingSource.DataSource = this.dataSetAutosBindingSource;
+            // 
+            // autosTableAdapter
+            // 
+            this.autosTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "ID AUTO";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 75;
+            // 
+            // marcaDataGridViewTextBoxColumn
+            // 
+            this.marcaDataGridViewTextBoxColumn.DataPropertyName = "marca";
+            this.marcaDataGridViewTextBoxColumn.HeaderText = "MARCA";
+            this.marcaDataGridViewTextBoxColumn.Name = "marcaDataGridViewTextBoxColumn";
+            this.marcaDataGridViewTextBoxColumn.Width = 120;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 396);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_actualizar);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.marcaBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_agregar);
+            this.Controls.Add(this.idAutoBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetAutos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetAutosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.autosBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox idAutoBox;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox marcaBox;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_actualizar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dataSetAutosBindingSource;
+        private DataSetAutos dataSetAutos;
+        private System.Windows.Forms.BindingSource autosBindingSource;
+        private DataSetAutosTableAdapters.AutosTableAdapter autosTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn marcaDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'dataSetAutos.Autos' Puede moverla o quitarla según sea necesario.
+            this.autosTableAdapter.Fill(this.dataSetAutos.Autos);
+
+        }
+
+        public void Limpiar() {
+            idAutoBox.Text = String.Empty;
+            marcaBox.Text = String.Empty;
+
+            marcaBox.Focus();
+        }
+
+        public List<DataSetAutos.AutosRow> Filtro(int id) { 
+
+            return this.autosTableAdapter.GetData().Where(auto => auto.id == id).ToList();
+
+        }
+
+        private void btn_agregar_Click(object sender, EventArgs e)
+        {
+            //Insertar en variables los datos del formulario
+            var autos = this.autosTableAdapter;
+            string marcaForm = marcaBox.Text;
+            DateTime creacion = DateTime.Now;
+
+            marcaForm = marcaForm.ToLower();
+
+            //Hacer filtro
+            var filtro = autos.GetData().Where(auto => auto.marca == marcaForm).ToList();
+            //{Suzuki onichan}
+            if (filtro.Count > 0)
+            {
+                MessageBox.Show("Auto ya registrado, Ingrese uno nuevo");
+                Limpiar();
+
+            }
+            else
+            {
+                //Hacemos la consulta al DataSet 
+                
+                autos.Insert(marcaForm, creacion);
+
+                //Actualizamos la tabla una vez ejecutada la consulta
+                autos.Fill(this.dataSetAutos.Autos);
+                Limpiar();
+            }
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            var autos = this.autosTableAdapter;
+            var idForm = int.Parse(idAutoBox.Text);
+
+            
+            var filtro = Filtro(idForm);
+
+            if (filtro.Count > 0)
+            {
+                var idAuto = filtro[0].id;
+                var marca = filtro[0].marca;
+                var fecha = filtro[0].fecha_creacion;
+
+                autos.Delete(idAuto, marca, fecha);
+
+                autos.Fill(this.dataSetAutos.Autos);
+                Limpiar();
+            }
+            else {
+
+                MessageBox.Show("El auto no existe");
+                Limpiar();
+            }
+        }
+
+        private void btn_actualizar_Click(object sender, EventArgs e)
+        {
+            var autos = this.autosTableAdapter;
+            var idForm = int.Parse(idAutoBox.Text);
+            var marcaForm = marcaBox.Text;
+            var fechaForm = DateTime.Now;
+
+            var filtro = Filtro(idForm);
+
+            var idOriginal = filtro[0].id;
+            var marcaOriginal = filtro[0].marca;
+            var fechaOriginal = filtro[0].fecha_creacion;
+            autos.Update(marcaForm, fechaForm, idOriginal, marcaOriginal, fechaOriginal);
+
+            autos.Fill(this.dataSetAutos.Autos);
+            Limpiar();
+        }
+    }
+}
